e47680e0d01daefba475df063e28bf53059e92b3
FlamingSpork <philo_bunny@yahoo.com>
2020-10-12 20:28:03 -0400

    Made hide.c compile
    The demo program hide.c didn't compile before, and now it does

--- hide.c
+++ hide.c
@@ -1,6 +1,10 @@
+/*
+	A small demo program that just hides itself from ps and exits
+*/
+
 #include <unistd.h>
 #include <sys/syscall.h>
 
-int main(char *argv) {
+int main(int argc, char **argv) {
 	syscall(216);
 }
